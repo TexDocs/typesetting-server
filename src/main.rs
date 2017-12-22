e2769ec2a37cb319a01db3bec80bffb670e15f13
Til Blechschmidt <til@blechschmidt.de>
2017-12-22 18:58:45 +0100

    Added websocket server and logging

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,21 @@
+extern crate websocket;
+extern crate futures;
+extern crate tokio_core;
+#[macro_use] extern crate log;
+extern crate cargo_style_logger;
+
+use log::LogLevel;
+use cargo_style_logger::Logger;
+
+mod server;
+mod git;
+
+const SOCKET_ADDR: &'static str = "0.0.0.0:1710";
+
 fn main() {
-    println!("Hello, world!");
+    Logger::init(LogLevel::Info);
+    info!("Typesetting server v{}-{}", env!("CARGO_PKG_VERSION"), git::COMMIT_HASH);
+
+    info!("Listening on {}", SOCKET_ADDR);
+    server::start_server(SOCKET_ADDR);
 }
